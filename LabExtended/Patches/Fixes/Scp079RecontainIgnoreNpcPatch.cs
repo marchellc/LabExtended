5a8213598ed56cd8ed29b3bd9d59fca8225933c3
Mallifrey <xsokolar@fi.muni.cz>
2024-08-19 18:25:49 +0200

    fix: ignore NPCs, when doing auto-overcharge

--- LabExtended/Patches/Fixes/Scp079RecontainIgnoreNpcPatch.cs
+++ LabExtended/Patches/Fixes/Scp079RecontainIgnoreNpcPatch.cs
@@ -10,15 +10,18 @@ namespace LabExtended.Patches.Fixes
     [HarmonyPatch(typeof(Scp079Recontainer), nameof(Scp079Recontainer.OnServerRoleChanged))]
     public static class Scp079RecontainIgnoreNpcPatch
     {
-        public static bool Prefix(Scp079Recontainer __instance, ReferenceHub hub, RoleTypeId newRole, RoleChangeReason reason)
-        {
+        public static bool Prefix(Scp079Recontainer __instance, ReferenceHub hub, RoleTypeId newRole)
+            {
+            if (!ExPlayer.TryGet(hub, out var player) || player.IsNpc)
+                return false;
+
             if (newRole != RoleTypeId.Spectator || !__instance.IsScpButNot079(hub.roleManager.CurrentRole))
                 return false;
 
-            if (!Scp079Role.ActiveInstances.Any(x => x.TryGetOwner(out var owner) && ExPlayer.TryGet(owner, out var player) && player.Switches.CanBeRecontainedAs079))
+            if (!Scp079Role.ActiveInstances.Any(x => x.TryGetOwner(out var owner) && ExPlayer.TryGet(owner, out var player) && !player.IsNpc && player.Switches.CanBeRecontainedAs079))
                 return false;
 
-            if (ReferenceHub.AllHubs.Count(x => x != hub && __instance.IsScpButNot079(x.roleManager.CurrentRole)) > 0)
+            if (ReferenceHub.AllHubs.Any(x => x != hub && __instance.IsScpButNot079(x.roleManager.CurrentRole) && ExPlayer.TryGet(x, out var player) && !player.IsNpc))
                 return false;
 
             __instance.SetContainmentDoors(true, true);
