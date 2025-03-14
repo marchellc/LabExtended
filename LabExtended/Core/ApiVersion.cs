e2aae65c787915bb7bfc366d4a21a503d62b52ee
marchellc <generalkenobi700@gmail.com>
2025-03-14 20:48:32 +0100

    More docs & ApiCommands removal as it's no longer needed.

--- LabExtended/Core/ApiVersion.cs
+++ LabExtended/Core/ApiVersion.cs
@@ -2,29 +2,57 @@
 
 namespace LabExtended.Core
 {
+    /// <summary>
+    /// Loader version handler.
+    /// </summary>
     public static class ApiVersion
     {
+        /// <summary>
+        /// The major version part.
+        /// </summary>
         public const int Major = 1;
+        
+        /// <summary>
+        /// The minor version part.
+        /// </summary>
         public const int Minor = 0;
+        
+        /// <summary>
+        /// The build version part.
+        /// </summary>
         public const int Build = 0;
+        
+        /// <summary>
+        /// The patch version part.
+        /// </summary>
         public const int Patch = 0;
 
+        /// <summary>
+        /// Gets the loader's current version.
+        /// </summary>
         public static Version Version { get; } = new(Major, Minor, Build, Patch);
+        
+        /// <summary>
+        /// Gets the game's current version.
+        /// </summary>
         public static Version Game { get; } = new(GameCore.Version.Major, GameCore.Version.Minor, GameCore.Version.Revision);
 
+        /// <summary>
+        /// Gets the loader's game version compatibility.
+        /// </summary>
         public static VersionRange? Compatibility { get; } = new VersionRange(new Version(14, 0, 3));
 
+        /// <summary>
+        /// Checks for server version compatibility.
+        /// </summary>
+        /// <returns>true if this loader version is compatible with this server version.</returns>
         public static bool CheckCompatibility()
         {
             if (ApiLoader.BaseConfig.SkipGameCompatibility) return true;
-
-            if (Compatibility.HasValue && !Compatibility.Value.InRange(Game))
-            {
-                ApiLog.Error("Extended Loader", $"Attempted to load for an unsupported game version (&1{Game}&r) - supported: &2{Compatibility.Value}&r");
-                return false;
-            }
-
-            return true;
+            if (!Compatibility.HasValue || Compatibility.Value.InRange(Game)) return true;
+            
+            ApiLog.Error("LabExtended", $"Attempted to load for an unsupported game version (&1{Game}&r) - supported: &2{Compatibility.Value}&r");
+            return false;
         }
     }
 }
