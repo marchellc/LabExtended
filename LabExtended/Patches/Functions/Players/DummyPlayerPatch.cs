17d5079b9e9bd1d39106a3f82db9dccea6a0ae7d
Mallifrey <xsokolar@fi.muni.cz>
2025-03-19 00:33:05 +0100

    Creating NPC (Dummy) ExPlayer instance

---
+++ LabExtended/Patches/Functions/Players/DummyPlayerPatch.cs
@@ -0,0 +1,13 @@
+﻿using GameCore;
+using HarmonyLib;
+using LabExtended.API;
+using LabExtended.API.Containers;
+namespace LabExtended.Patches.Functions.Players;
+
+public static class DummyPlayerPatch {
+    [HarmonyPatch(typeof(DummyUtils), nameof(DummyUtils.SpawnDummy))]
+    public static void SpawnPostfix(ref ReferenceHub __result)
+    {
+        _ = new ExPlayer(__result, SwitchContainer.GetNewNpcToggles(true));
+    }
+}
