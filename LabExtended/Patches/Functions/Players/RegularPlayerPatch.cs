027b3303d7ce5773f17b6d5e25f8f7058cec7f02
Mallifrey <xsokolar@fi.muni.cz>
2025-03-19 00:32:12 +0100

    Properly creating ExPlayer instance instead of Player

--- LabExtended/Patches/Functions/Players/RegularPlayerPatch.cs
+++ LabExtended/Patches/Functions/Players/RegularPlayerPatch.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
-
+using LabApi.Events.Arguments.PlayerEvents;
+using LabApi.Events.Handlers;
 using LabApi.Features.Wrappers;
 
 using LabExtended.API;
@@ -7,17 +8,26 @@ using LabExtended.Events;
 
 namespace LabExtended.Patches.Functions.Players;
 
-public static class RegularPlayerPatch
-{
-    [HarmonyPatch(typeof(Player), nameof(Player.AddPlayer))]
-    public static bool JoinPrefix(ReferenceHub referenceHub)
-    {
-        if (referenceHub != null && !referenceHub.isLocalPlayer)
-            _ = new ExPlayer(referenceHub);
+public static class RegularPlayerPatch {
+    [HarmonyPatch(typeof(PlayerEvents), nameof(PlayerEvents.OnJoined))]
+    public static void OnJoinedPostfix(PlayerJoinedEventArgs ev) {
+        if (ev.Player is ExPlayer exPlayer)
+            InternalEvents.HandlePlayerJoin(exPlayer);
+    }
 
+    [HarmonyPatch(typeof(Player), nameof(Player.Get), [typeof(ReferenceHub)])]
+    public static bool PlayerGetPrefix(ReferenceHub? referenceHub, ref Player? __result) {
+        if (referenceHub == null) {
+            __result = null;
+            return false;
+        }
+        __result = Player.Dictionary.TryGetValue(referenceHub, out Player player) ? player : new ExPlayer(referenceHub);
         return false;
     }
-    
+
+    [HarmonyPatch(typeof(Player), nameof(Player.AddPlayer), [typeof(ReferenceHub)])]
+    public static bool AddPlayerPrefix() => false;
+
     [HarmonyPatch(typeof(Player), nameof(Player.RemovePlayer))]
     public static bool LeavePrefix(ReferenceHub referenceHub)
     {
