1daa4f0688f31bd15320df9dc565f5602bd6fca1
Mallifrey <xsokolar@fi.muni.cz>
2025-03-21 18:14:09 +0100

    Revert "Added missing InternalEvents.HandlePlayerJoin"
    This reverts commit 019379452e8721d1f4ec1d0660acd05d5b5db82e.

--- LabExtended/Patches/Functions/Players/RegularPlayerPatch.cs
+++ LabExtended/Patches/Functions/Players/RegularPlayerPatch.cs
@@ -1,13 +1,8 @@
 ﻿using HarmonyLib;
-using System.Reflection.Emit;
-using System.Reflection;
 
-using CentralAuth;
-using LabApi.Events.Arguments.PlayerEvents;
 using LabApi.Features.Wrappers;
 
 using LabExtended.API;
-using LabExtended.Core;
 using LabExtended.Events;
 
 namespace LabExtended.Patches.Functions.Players;
@@ -22,28 +17,7 @@ public static class RegularPlayerPatch
 
         return false;
     }
-
-    [HarmonyPatch(typeof(PlayerAuthenticationManager), nameof(PlayerAuthenticationManager.ProcessAuthenticationResponse), [typeof(AuthenticationResponse)])]
-    static IEnumerable<CodeInstruction> InvokePlayerJoinTranspiler(IEnumerable<CodeInstruction> instructions) {
-        var code = new List<CodeInstruction>(instructions);
-
-        for (int i = 0; i < code.Count; i++) {
-            if (code[i].opcode == OpCodes.Newobj &&
-               code[i].operand as ConstructorInfo == AccessTools.Constructor(typeof(PlayerJoinedEventArgs), [typeof(ReferenceHub)])) {
-                code.InsertRange(i, [
-                    new(OpCodes.Ldarg_0),
-                    new(OpCodes.Ldfld, AccessTools.Field(typeof(PlayerAuthenticationManager), nameof(PlayerAuthenticationManager._hub))),
-                    new(OpCodes.Call, AccessTools.Method(typeof(ExPlayer), nameof(ExPlayer.Get), [typeof(ReferenceHub)])),
-                    new(OpCodes.Call, AccessTools.Method(typeof(InternalEvents), nameof(InternalEvents.HandlePlayerJoin), [typeof(ExPlayer)]))
-                ]);
-                return code;
-            }
-        }
-
-        ApiLog.Error("Couldn't find Insertion index in " + nameof(InvokePlayerJoinTranspiler));
-        return code;
-    }
-
+    
     [HarmonyPatch(typeof(Player), nameof(Player.RemovePlayer))]
     public static bool LeavePrefix(ReferenceHub referenceHub)
     {
