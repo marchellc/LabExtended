3d54079879bf6e5c4ddceefe4c71dfcd7b066b4c
marchellc <generalkenobi700@gmail.com>
2025-05-01 18:09:43 +0200

    Implemented the PreventsRecontaining079 toggle.

---
+++ LabExtended/Patches/Functions/Scp079/Scp079PreventRecontainmentPatch.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+
+using LabExtended.API;
+using LabExtended.API.Containers;
+
+using PlayerRoles;
+using PlayerRoles.PlayableScps.Scp079;
+
+namespace LabExtended.Patches.Functions.Scp079;
+
+/// <summary>
+/// Implements the <see cref="SwitchContainer.PreventsRecontaining079"/> toggle.
+/// </summary>
+public static class Scp079PreventRecontainmentPatch
+{
+    [HarmonyPatch(typeof(Scp079Recontainer), nameof(Scp079Recontainer.IsScpButNot079))]
+    private static bool Prefix(PlayerRoleBase prb, ref bool __result)
+    {
+        if (prb.TryGetOwner(out var owner)
+            && ExPlayer.TryGet(owner, out var player)
+            && player.Toggles.PreventsRecontaining079)
+            return __result = false;
+
+        __result = prb.Team is Team.SCPs && prb.RoleTypeId is not RoleTypeId.Scp079;
+        return false;
+    }
+}
