3d51a8d1682cd9adc19a4de36d3039558e1ee2f8
marchellc <generalkenobi700@gmail.com>
2025-05-17 10:55:29 +0200

    Added new text toy image commands

---
+++ LabExtended/Commands/Custom/Text/Enable/EnableCommand.cs
@@ -0,0 +1,25 @@
+﻿using LabExtended.API.Toys;
+using LabExtended.Commands.Attributes;
+
+using LabExtended.Images.Playback;
+
+namespace LabExtended.Commands.Custom.TextToy;
+
+public partial class TextCommand
+{
+    [CommandOverload("enable", "Enables a playback option.")]
+    public void EnableOverload(
+        [CommandParameter("ID", "ID of the text toy.")] uint toyId, 
+        [CommandParameter("Option", "The option to enable.")] PlaybackFlags option)
+    {
+        if (!AdminToy.TryGet<API.Toys.TextToy>(x => x.NetId == toyId, out var textToy))
+        {
+            Fail($"Unknown ID: {toyId}");
+            return;
+        }
+
+        textToy.PlaybackDisplay.EnableOption(option);
+        
+        Ok($"Enabled option '{option}' on toy '{toyId}' (options: {textToy.PlaybackDisplay.Options})");
+    }
+}
