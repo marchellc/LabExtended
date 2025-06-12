9b83277eb5308baeaa85b3eec5ad2121df243f9c
marchellc <generalkenobi700@gmail.com>
2025-06-12 14:09:17 +0200

    Removed the SCP-079 SpawningPing & SpawnedPing events in favor of LabAPI.

--- LabExtended/Events/ExScp079Events.cs
+++ LabExtended/Events/ExScp079Events.cs
@@ -12,12 +12,6 @@ public static class ExScp079Events
 {
     /// <inheritdoc cref="Scp079RecontainingEventArgs"/>.
     public static event Action<Scp079RecontainingEventArgs>? Recontaining;
-    
-    /// <inheritdoc cref="Scp079SpawningPingEventArgs"/>
-    public static event Action<Scp079SpawningPingEventArgs>? SpawningPing;
-
-    /// <inheritdoc cref="Scp079SpawnedPingEventArgs"/>
-    public static event Action<Scp079SpawnedPingEventArgs>? SpawnedPing;
 
     /// <summary>
     /// Invokes the <see cref="Recontaining"/> event.
@@ -26,19 +20,4 @@ public static class ExScp079Events
     /// <returns>The <see cref="BooleanEventArgs.IsAllowed"/> property.</returns>
     public static bool OnRecontaining(Scp079RecontainingEventArgs args)
         => Recontaining.InvokeBooleanEvent(args);
-    
-    /// <summary>
-    /// Invokes the <see cref="SpawningPing"/> event.
-    /// </summary>
-    /// <param name="args">The event's arguments.</param>
-    /// <returns>The <see cref="BooleanEventArgs.IsAllowed"/> property.</returns>
-    public static bool OnSpawningPing(Scp079SpawningPingEventArgs args)
-        => SpawningPing.InvokeBooleanEvent(args);
-    
-    /// <summary>
-    /// Invokes the <see cref="SpawnedPing"/> event.
-    /// </summary>
-    /// <param name="args">The event's arguments.</param>
-    public static void OnSpawnedPing(Scp079SpawnedPingEventArgs args)
-        => SpawnedPing.InvokeEvent(args);
 }
