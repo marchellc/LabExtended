cdd4e5690fb83649f1653caa651c8647bfce5810
marchellc <generalkenobi700@gmail.com>
2025-07-20 11:00:04 +0200

    Removed an unused config value.

--- LabExtended/Core/Configs/Sections/OtherSection.cs
+++ LabExtended/Core/Configs/Sections/OtherSection.cs
@@ -6,9 +6,6 @@ namespace LabExtended.Core.Configs.Sections
 {
     public class OtherSection
     {
-        [Description("Whether or not to enable custom SCP-914 recipes. This is a feature in testing and may break a lot of things.")]
-        public bool Scp914CustomRecipes { get; set; }
-        
         [Description("A collection of properties used to control behaviour specific to real players.")]
         public SwitchContainer PlayerToggles { get; set; } = SwitchContainer.DefaultPlayerSwitches;
         
