dab088ab2d693bfa0ff26e8c83b7fe8bd9839b3c
marchellc <generalkenobi700@gmail.com>
2025-07-31 13:17:43 +0200

    view command docs

--- LabExtended/Commands/Custom/View/ViewCommand.cs
+++ LabExtended/Commands/Custom/View/ViewCommand.cs
@@ -3,5 +3,8 @@ using LabExtended.Commands.Interfaces;
 
 namespace LabExtended.Commands.Custom.View;
 
+/// <summary>
+/// Base command for object descriptions.
+/// </summary>
 [Command("view", "Views the description of an object.")]
 public partial class ViewCommand : CommandBase, IServerSideCommand { }
