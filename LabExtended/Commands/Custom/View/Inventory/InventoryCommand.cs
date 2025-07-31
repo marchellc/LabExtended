96149fdde06e2a5f6258bd5df17fc36b4d785610
marchellc <generalkenobi700@gmail.com>
2025-07-31 13:16:31 +0200

    Added a command overload to the view command to display all items in a player's inventory.

---
+++ LabExtended/Commands/Custom/View/Inventory/InventoryCommand.cs
@@ -0,0 +1,92 @@
+using InventorySystem.Items;
+using InventorySystem.Items.Firearms;
+using InventorySystem.Items.Firearms.Modules;
+
+using LabExtended.API;
+using LabExtended.API.CustomItems;
+using LabExtended.API.CustomItems.Behaviours;
+
+using LabExtended.Extensions;
+
+using LabExtended.Commands.Attributes;
+using LabExtended.Utilities.Firearms;
+
+namespace LabExtended.Commands.Custom.View;
+
+public partial class ViewCommand
+{
+    /// <summary>
+    /// Lists all items and ammo in a player's inventory.
+    /// </summary>
+    [CommandOverload("inv", "Lists all items in a player's inventory.")]
+    public void Inventory(
+        [CommandParameter("Target", "The targeted player (defaults to you).")] ExPlayer? target = null)
+    {
+        target ??= Sender;
+
+        if (target.Inventory.ItemCount < 1 && !target.Ammo.HasAnyAmmo)
+        {
+            Fail($"Inventory of player {target.ToCommandString()} is empty.");
+            return;
+        }
+        
+        Ok(x =>
+        {
+            x.AppendLine($"Listing inventory of player {target.ToCommandString()}:");
+
+            if (target.Inventory.ItemCount > 0)
+            {
+                x.AppendLine("- Items");
+
+                foreach (var item in target.Inventory.Items)
+                {
+                    x.AppendLine($"  > ({item.GetInventorySlot()}) [{item.ItemSerial}] {item.ItemTypeId}");
+
+                    if (target.Inventory.CurrentItem != null && target.Inventory.CurrentItem == item)
+                    {
+                        x.AppendLine("    - Held Item");
+                    }
+                    
+                    if (item is Firearm firearm)
+                    {
+                        if (firearm.TryGetModule<IPrimaryAmmoContainerModule>(out var ammoContainer))
+                        {
+                            x.AppendLine($"    - Firearm Ammo: {ammoContainer!.AmmoStored} / {ammoContainer.AmmoMax} ({ammoContainer.AmmoType})");
+                        }
+                    }
+
+                    if (item is ILightEmittingItem lightEmittingItem)
+                    {
+                        x.AppendLine($"    - Emitting Light: {lightEmittingItem.IsEmittingLight}");
+                    }
+
+                    if (CustomItemUtils.TryGetBehaviour<CustomItemInventoryBehaviour>(item.ItemSerial, out var behaviour)
+                        && behaviour?.Handler != null)
+                    {
+                        x.AppendLine($"    - Custom Item: {behaviour.Handler.Name} ({behaviour.Handler.Id})");
+                    }
+                }
+            }
+
+            if (target.Ammo.HasAnyAmmo)
+            {
+                x.AppendLine("- Ammo");
+
+                foreach (var pair in target.Ammo.Ammo)
+                {
+                    x.AppendLine($"  > {pair.Key}: {pair.Value}");
+                }
+            }
+
+            if (target.Inventory.CustomAmmo.Ammo.Count > 0)
+            {
+                x.AppendLine("- Custom Ammo");
+
+                foreach (var pair in target.Inventory.CustomAmmo.Ammo)
+                {
+                    x.AppendLine($"  > {pair.Key}: {pair.Value}");
+                }
+            }
+        });
+    }
+}
