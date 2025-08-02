a8340bd8ba31e057ad1ce63c04439b0aa7257c3f
marchellc <generalkenobi700@gmail.com>
2025-08-02 10:47:43 +0200

    Removed configs that were unused (or didn't work) and added docs and a boolean config unload_plugins_on_quit which toggles unloading plugins when the server stops.

--- LabExtended/Core/Configs/BaseConfig.cs
+++ LabExtended/Core/Configs/BaseConfig.cs
@@ -3,46 +3,72 @@ using System.ComponentModel;
 using LabExtended.Patches.Functions.Players;
 using LabExtended.Utilities;
 
-namespace LabExtended.Core.Configs
+namespace LabExtended.Core.Configs;
+
+/// <summary>
+/// The base config of LabExtended
+/// </summary>
+public class BaseConfig
 {
-    public class BaseConfig
+    /// <summary>
+    /// Whether or not to show debug messages.
+    /// </summary>
+    [Description("Toggles logging of debug messages.")]
+    public bool DebugEnabled { get; set; }
+
+    /// <summary>
+    /// Whether or not to show transpiler debug logs (this includes transpilers of other plugins).
+    /// </summary>
+    [Description("Toggles debug logs of transpilers.")]
+    public bool TranspilerDebugEnabled { get; set; }
+
+    /// <summary>
+    /// Whether or not to add true color tags to logs.
+    /// </summary>
+    [Description("Toggles true color log formatting.")]
+    public bool TrueColorEnabled { get; set; } = true;
+
+    /// <summary>
+    /// Whether or not to disable round lock once the player who enabled it leaves the server.
+    /// </summary>
+    [Description("Whether or not to disable Round Lock when the player who enabled it leaves.")]
+    public bool DisableRoundLockOnLeave { get; set; } = true;
+
+    /// <summary>
+    /// Whether or not to disable lobby lock once the player who enabled it leaves the server.
+    /// </summary>
+    [Description("Whether or not to disable Lobby Lock when the player who enabled it leaves.")]
+    public bool DisableLobbyLockOnLeave { get; set; } = true;
+
+    /// <summary>
+    /// Whether or not to unload all plugins once the server's process quits.
+    /// </summary>
+    [Description("Whether or not to unload all plugins once the server's process quits.")]
+    public bool UnloadPluginsOnQuit { get; set; } = true;
+
+    /// <summary>
+    /// The maximum distance between a disarmer and a disarmed player before being automatically uncuffed.
+    /// </summary>
+    [Description("The maximum distance between a disarmer and a disarmed player before being automatically uncuffed.")]
+    public float RemoveDisarmRange
     {
-        [Description("Toggles logging of debug messages.")]
-        public bool DebugEnabled { get; set; }
-        
-        [Description("Toggles debug logs of transpilers.")]
-        public bool TranspilerDebugEnabled { get; set; }
-
-        [Description("Toggles true color log formatting.")]
-        public bool TrueColorEnabled { get; set; } = true;
-
-        [Description("Whether or not to skip game version compatibility checks.")]
-        public bool SkipGameCompatibility { get; set; }
-        
-        [Description("Whether or not to skip the client version compatibility checks.")]
-        public bool SkipClientCompatibility { get; set; }
-
-        [Description("Whether or not to disable Round Lock when the player who enabled it leaves.")]
-        public bool DisableRoundLockOnLeave { get; set; } = true;
-
-        [Description("Whether or not to disable Lobby Lock when the player who enabled it leaves.")]
-        public bool DisableLobbyLockOnLeave { get; set; } = true;
-
-        [Description("The maximum distance between a disarmer and a disarmed player before being automatically uncuffed.")]
-        public float RemoveDisarmRange
-        {
-            get => DisarmValidateEntryPatch.DisarmDistance;
-            set => DisarmValidateEntryPatch.DisarmDistance = value;
-        }
-
-        [Description("A list of named positions plugins can use to spawn objects consistently across map seeds.")]
-        public Dictionary<string, MapUtilities.NamedPosition> Positions { get; set; } = new()
-        {
-            ["example"] = new(),
-            ["example2"] = new(),
-        };
-
-        [Description("Sets a list of sources that cannot send debug messages.")]
-        public List<string> DisabledDebugSources { get; set; } = new();
+        get => DisarmValidateEntryPatch.DisarmDistance;
+        set => DisarmValidateEntryPatch.DisarmDistance = value;
     }
+
+    /// <summary>
+    /// A list of named positions plugins can use to spawn objects consistently across map seeds.
+    /// </summary>
+    [Description("A list of named positions plugins can use to spawn objects consistently across map seeds.")]
+    public Dictionary<string, MapUtilities.NamedPosition> Positions { get; set; } = new()
+    {
+        ["example"] = new(),
+        ["example2"] = new(),
+    };
+
+    /// <summary>
+    /// A list of source names which will not be logged using debug logs.
+    /// </summary>
+    [Description("Sets a list of sources that cannot send debug messages.")]
+    public List<string> DisabledDebugSources { get; set; } = new();
 }
