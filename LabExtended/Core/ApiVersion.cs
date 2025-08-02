58d6f6b1e995ffa1ce3329e363be63a68db89451
marchellc <generalkenobi700@gmail.com>
2025-08-02 10:48:44 +0200

    Changed the compatible server version

--- LabExtended/Core/ApiVersion.cs
+++ LabExtended/Core/ApiVersion.cs
@@ -1,58 +1,59 @@
 ﻿using LabExtended.Utilities;
 
-namespace LabExtended.Core
+namespace LabExtended.Core;
+
+/// <summary>
+/// Loader version handler.
+/// </summary>
+public static class ApiVersion
 {
     /// <summary>
-    /// Loader version handler.
+    /// The major version part.
     /// </summary>
-    public static class ApiVersion
+    public const int Major = 1;
+
+    /// <summary>
+    /// The minor version part.
+    /// </summary>
+    public const int Minor = 0;
+
+    /// <summary>
+    /// The build version part.
+    /// </summary>
+    public const int Build = 0;
+
+    /// <summary>
+    /// The patch version part.
+    /// </summary>
+    public const int Patch = 0;
+
+    /// <summary>
+    /// Gets the loader's current version.
+    /// </summary>
+    public static Version Version { get; } = new(Major, Minor, Build, Patch);
+
+    /// <summary>
+    /// Gets the game's current version.
+    /// </summary>
+    public static Version Game { get; } =
+        new(GameCore.Version.Major, GameCore.Version.Minor, GameCore.Version.Revision);
+
+    /// <summary>
+    /// Gets the loader's game version compatibility.
+    /// </summary>
+    public static VersionRange? Compatibility { get; } = new VersionRange(new Version(14, 1, 3));
+
+    /// <summary>
+    /// Checks for server version compatibility.
+    /// </summary>
+    /// <returns>true if this loader version is compatible with this server version.</returns>
+    public static bool CheckCompatibility()
     {
-        /// <summary>
-        /// The major version part.
-        /// </summary>
-        public const int Major = 1;
-        
-        /// <summary>
-        /// The minor version part.
-        /// </summary>
-        public const int Minor = 0;
-        
-        /// <summary>
-        /// The build version part.
-        /// </summary>
-        public const int Build = 0;
-        
-        /// <summary>
-        /// The patch version part.
-        /// </summary>
-        public const int Patch = 0;
+        if (!Compatibility.HasValue || Compatibility.Value.InRange(Game)) 
+            return true;
 
-        /// <summary>
-        /// Gets the loader's current version.
-        /// </summary>
-        public static Version Version { get; } = new(Major, Minor, Build, Patch);
-        
-        /// <summary>
-        /// Gets the game's current version.
-        /// </summary>
-        public static Version Game { get; } = new(GameCore.Version.Major, GameCore.Version.Minor, GameCore.Version.Revision);
-
-        /// <summary>
-        /// Gets the loader's game version compatibility.
-        /// </summary>
-        public static VersionRange? Compatibility { get; } = new VersionRange(new Version(14, 1, 1));
-
-        /// <summary>
-        /// Checks for server version compatibility.
-        /// </summary>
-        /// <returns>true if this loader version is compatible with this server version.</returns>
-        public static bool CheckCompatibility()
-        {
-            if (ApiLoader.BaseConfig.SkipGameCompatibility) return true;
-            if (!Compatibility.HasValue || Compatibility.Value.InRange(Game)) return true;
-            
-            ApiLog.Error("LabExtended", $"Attempted to load for an unsupported game version (&1{Game}&r) - supported: &2{Compatibility.Value}&r");
-            return false;
-        }
+        ApiLog.Error("LabExtended",
+            $"Attempted to load for an unsupported game version (&1{Game}&r) - supported: &2{Compatibility.Value}&r");
+        return false;
     }
 }
