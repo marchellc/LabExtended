5dcc4a78adfa2fefb7138e9c0a0d4a3cb6ec874a
marchellc <generalkenobi700@gmail.com>
2025-08-02 13:10:40 +0200

    Modified the RegularPlayerPatch (now PlayerConstructorPatch) to set the host player as well.

---
+++ LabExtended/Patches/Functions/Players/PlayerConstructorPatch.cs
@@ -0,0 +1,66 @@
+﻿using HarmonyLib;
+
+using LabApi.Features.Stores;
+
+using Mirror;
+
+using LabApi.Features.Wrappers;
+
+using LabExtended.API;
+using LabExtended.Events;
+
+using static LabExtended.API.Containers.SwitchContainer;
+
+namespace LabExtended.Patches.Functions.Players;
+
+/// <summary>
+/// Used to replace LabAPI's <see cref="Player"/> class with our custom <see cref="ExPlayer"/> subtype.
+/// </summary>
+public static class PlayerConstructorPatch
+{
+    [HarmonyPatch(typeof(Player), nameof(Player.AddPlayer))]
+    private static bool JoinPrefix(ReferenceHub referenceHub)
+    {
+        if (referenceHub != null)
+        {
+            var player = new ExPlayer(referenceHub,
+                referenceHub.isLocalPlayer || referenceHub.connectionToClient.GetType() != typeof(NetworkConnectionToClient)
+                    ? GetNewNpcToggles()
+                    : GetNewPlayerToggles());
+
+            if (referenceHub.isLocalPlayer)
+            {
+                ExPlayer.host = player;
+                Server.Host = player;
+            }
+        }
+
+        return false;
+    }
+    
+    [HarmonyPatch(typeof(Player), nameof(Player.RemovePlayer))]
+    private static bool LeavePrefix(ReferenceHub referenceHub)
+    {
+        Player.Dictionary.Remove(referenceHub);
+        
+        if (referenceHub.authManager.UserId != null)
+            Player.UserIdCache.Remove(referenceHub.authManager.UserId);
+        
+        if (referenceHub.isLocalPlayer)
+        {
+            ExPlayer.host = null;
+            Server.Host = null;
+        }
+        
+        if (ExPlayer.TryGet(referenceHub, out var player) && player != null)
+        {
+            CustomDataStoreManager.RemovePlayer(player);
+            
+            InternalEvents.HandlePlayerLeave(player);
+            
+            player.Dispose();
+        }
+
+        return false;
+    }
+}
