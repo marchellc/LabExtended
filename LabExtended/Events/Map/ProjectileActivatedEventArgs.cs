174d33755219e152d6f24bd1298a1ba23eb248f0
marchellc <generalkenobi700@gmail.com>
2025-10-03 16:47:38 +0200

    ProjectileActivatedEventArgs

---
+++ LabExtended/Events/Map/ProjectileActivatedEventArgs.cs
@@ -0,0 +1,34 @@
+﻿using InventorySystem.Items.ThrowableProjectiles;
+
+namespace LabExtended.Events.Map
+{
+    /// <summary>
+    /// Gets called after an inactive projectile gets activated by another explosion.
+    /// </summary>
+    /// <remarks>This event is called <b>before</b> the spawned projectile is activated by calling <see cref="ThrownProjectile.ServerActivate"/></remarks>
+    public class ProjectileActivatedEventArgs : BooleanEventArgs
+    {
+        /// <summary>
+        /// Gets the instance of the projectile which was activated.
+        /// </summary>
+        public TimedGrenadePickup Pickup { get; }
+
+        /// <summary>
+        /// Gets the projectile which was spawned.
+        /// </summary>
+        public ThrownProjectile Projectile { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the ProjectileActivatedEventArgs class with the specified pickup and
+        /// projectile information.
+        /// </summary>
+        /// <param name="pickup">The TimedGrenadePickup instance representing the pickup that triggered the projectile activation. Cannot be
+        /// null.</param>
+        /// <param name="projectile">The ThrownProjectile instance representing the activated projectile. Cannot be null.</param>
+        public ProjectileActivatedEventArgs(TimedGrenadePickup pickup, ThrownProjectile projectile)
+        {
+            Pickup = pickup;
+            Projectile = projectile;
+        }
+    }
+}
---
+++ LabExtended/Patches/Events/Map/ProjectileActivatedPatch.cs
@@ -0,0 +1,67 @@
+﻿using HarmonyLib;
+
+using InventorySystem.Items.Pickups;
+using InventorySystem.Items.ThrowableProjectiles;
+
+using LabExtended.Events;
+using LabExtended.Extensions;
+
+using Mirror;
+
+namespace LabExtended.Patches.Events.Map
+{
+    /// <summary>
+    /// Implements the <see cref="ExMapEvents.ProjectileActivated"/> event.
+    /// </summary>
+    public static class ProjectileActivatedPatch
+    {
+        [HarmonyPatch(typeof(TimedGrenadePickup), nameof(TimedGrenadePickup.Update))]
+        private static bool Prefix(TimedGrenadePickup __instance)
+        {
+            if (!__instance._replaceNextFrame)
+                return false;
+
+            if (!__instance.Info.ItemId.TryGetItemPrefab<ThrowableItem>(out var throwableItemTemplate)
+                || throwableItemTemplate == null)
+                return false;
+
+            var thrownProjectile = UnityEngine.Object.Instantiate(throwableItemTemplate.Projectile);
+            var thrownProjectilePhysics = thrownProjectile?.PhysicsModule as PickupStandardPhysics;
+
+            if (thrownProjectile != null)
+            {
+                if (thrownProjectilePhysics != null && __instance.PhysicsModule is PickupStandardPhysics itemPhysics)
+                {
+                    var projectileRigidbody = thrownProjectilePhysics.Rb;
+                    var itemRidigbody = itemPhysics.Rb;
+
+                    projectileRigidbody.position = itemRidigbody.position;
+                    projectileRigidbody.rotation = itemRidigbody.rotation;
+                    projectileRigidbody.linearVelocity = itemRidigbody.linearVelocity;
+                    projectileRigidbody.angularVelocity = itemRidigbody.angularVelocity;
+                }
+
+                __instance.Info.Locked = true;
+
+                thrownProjectile.NetworkInfo = __instance.Info;
+                thrownProjectile.PreviousOwner = __instance._attacker;
+
+                if (ExMapEvents.OnProjectileActivated(new(__instance, thrownProjectile)))
+                {
+                    NetworkServer.Spawn(thrownProjectile.gameObject);
+
+                    thrownProjectile.ServerActivate();
+                }
+                else
+                {
+                    UnityEngine.Object.Destroy(thrownProjectile.gameObject);
+                }
+
+                __instance.DestroySelf();
+                __instance._replaceNextFrame = false;
+            }
+
+            return false;
+        }
+    }
+}
