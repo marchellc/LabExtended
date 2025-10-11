30606bd41985b94f76bc75e350d99010424b5953
marchellc <generalkenobi700@gmail.com>
2025-10-11 20:57:55 +0200

    Added a patch to supress log spam from Mirror

---
+++ LabExtended/Patches/Functions/Mirror/MirrorLogPatches.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+
+using Mirror;
+
+namespace LabExtended.Patches.Functions.Mirror
+{
+    /// <summary>
+    /// Removes pointless Mirror log spam.
+    /// </summary>
+    public static class MirrorLogPatches
+    {
+        [HarmonyPatch(typeof(NetworkServer), nameof(NetworkServer.BroadcastToConnection))]
+        private static bool BroadcastToConnectionPrefix(NetworkConnectionToClient connection)
+        {
+            foreach (var networkIdentity in connection.observing)
+            {
+                if (networkIdentity != null)
+                {
+                    var networkWriter = NetworkServer.SerializeForConnection(networkIdentity, connection);
+
+                    if (networkWriter != null)
+                    {
+                        connection.Send(new EntityStateMessage
+                        {
+                            netId = networkIdentity.netId,
+                            payload = networkWriter.ToArraySegment()
+                        });
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
