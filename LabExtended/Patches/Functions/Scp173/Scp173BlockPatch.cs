4ebc3c7ec12c9bb4fe5c61ad325d7cde4de36a64
marchellc <generalkenobi700@gmail.com>
2025-10-21 09:24:28 +0200

    Updated Scp173BlockPatch

--- LabExtended/Patches/Functions/Scp173/Scp173BlockPatch.cs
+++ LabExtended/Patches/Functions/Scp173/Scp173BlockPatch.cs
@@ -1,6 +1,7 @@
 ﻿using HarmonyLib;
 
 using LabExtended.API;
+using LabExtended.API.Containers;
 
 using LabExtended.Events;
 using LabExtended.Events.Player;
@@ -14,10 +15,14 @@ using UnityEngine;
 
 namespace LabExtended.Patches.Functions.Scp173
 {
+    /// <summary>
+    /// Implements functionality of the <see cref="SwitchContainer.CanBeBlockedAs173"/> and <see cref="SwitchContainer.CanBlockScp173"/>, as
+    /// well as the <see cref="PlayerObservingScp173EventArgs"/> event.
+    /// </summary>
     public static class Scp173BlockPatch
     {
         [HarmonyPatch(typeof(Scp173ObserversTracker), nameof(Scp173ObserversTracker.IsObservedBy))]
-        public static bool Prefix(Scp173ObserversTracker __instance, ReferenceHub target, float widthMultiplier, ref bool __result)
+        private static bool Prefix(Scp173ObserversTracker __instance, ReferenceHub target, float widthMultiplier, ref bool __result)
         {
             var scp = ExPlayer.Get(__instance.Owner);
             var player = ExPlayer.Get(target);
@@ -32,8 +37,14 @@ namespace LabExtended.Patches.Functions.Scp173
                 return __result = false;
 
             var position = __instance.CastRole.FpcModule.Position;
+            var maxDistance = __instance._maxViewDistance;
             var roomIdentifier = position.TryGetRoom(out var room) ? room : null;
-            var vision = VisionInformation.GetVisionInformation(target, target.PlayerCameraReference, position, __instance._modelWidth, roomIdentifier != null && roomIdentifier.Zone == FacilityZone.Surface ? __instance._maxViewDistance * 2f : __instance._maxViewDistance, checkFog: false, checkLineOfSight: false);
+
+            if (room != null && room.Zone is FacilityZone.Surface)
+                maxDistance *= 2f;
+
+            var vision = VisionInformation.GetVisionInformation(target, target.PlayerCameraReference, position, __instance._modelWidth, maxDistance, 
+                false, false, 0, true);
 
             if (!vision.IsLooking)
                 return false;
@@ -45,16 +56,15 @@ namespace LabExtended.Patches.Functions.Scp173
             for (int i = 0; i < visibilityReferencePoints.Length; i++)
             {
                 var vector2 = visibilityReferencePoints[i];
+                var vector3 = position + vector2.x * widthMultiplier * vector + Vector3.up * vector2.y;
 
-                if (!Physics.Linecast(position + vector2.x * widthMultiplier * vector + Vector3.up * vector2.y, position2, 
-                        VisionInformation.VisionLayerMask))
+                if (!Physics.Linecast(vector3, position2, VisionInformation.VisionLayerMask)
+                    && !Physics.Linecast(position, vector3, VisionInformation.VisionLayerMask))
                 {
                     var lookingEv = new PlayerObservingScp173EventArgs(scp, player, __instance.CastRole, __instance, vision, vision.IsLooking);
 
-                    if (!ExPlayerEvents.OnObservingScp173(lookingEv))
-                        return __result = false;
-
-                    if (!lookingEv.IsLooking)
+                    if (!ExPlayerEvents.OnObservingScp173(lookingEv)
+                        || !lookingEv.IsLooking)
                         return __result = false;
 
                     __result = true;
