02f2da71704aca9bed8c56617eee9cb83fc33f7f
marchellc <generalkenobi700@gmail.com>
2025-11-14 20:25:09 +0100

    Added a  fix for the local server player timing out while authentificating - still unsure why this is happening

---
+++ LabExtended/Patches/Fixes/LocalAuthTimedOutFix.cs
@@ -0,0 +1,35 @@
+﻿using CentralAuth;
+
+using HarmonyLib;
+
+using LabExtended.Core;
+
+namespace LabExtended.Patches.Fixes
+{
+    /// <summary>
+    /// Provides a fix to prevent the local player from being incorrectly rejected due to authentication timeouts during
+    /// local or dedicated server sessions.
+    /// </summary>
+    public static class LocalAuthTimedOutFix
+    {
+        [HarmonyPatch(typeof(PlayerAuthenticationManager), nameof(PlayerAuthenticationManager.RejectAuthentication))]
+        private static bool RejectPrefix(PlayerAuthenticationManager __instance)
+        {
+            if (__instance != null
+                && (__instance.isLocalPlayer
+                    || __instance.connectionToClient.address == "localhost"))
+            {
+                ApiLog.Debug("LabExtended", "Attempted to reject authentification of the local player!");
+
+                __instance.UserId = "ID_Dedicated";
+
+                __instance._hub.nicknameSync.UpdateNickname("Dedicated Server");
+                __instance._hub.serverRoles.RefreshPermissions(false);
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
